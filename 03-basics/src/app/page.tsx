235bf403cf4b18a8bf298888a5ce5a2ad3a47d95
Anoushka Raghuveeran <anoushkaraghuveeran@gmail.com>
2025-12-29 19:02:43 +0530

    initialize Next.js project with Tailwind CSS and custom fonts
    - Add PostCSS configuration for Tailwind CSS
    - Include global CSS file with Tailwind directives
    - Create layout component with local font imports
    - Add favicon and custom font files
    - Implement a basic home page with a centered heading
    - Configure Tailwind CSS with custom colors and content paths
    - Set up TypeScript configuration for the project

---
+++ 03-basics/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='flex h-screen items-center justify-center'>hey, im learning nextjs</h1>
+    </div>
+  );
+}
